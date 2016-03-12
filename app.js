8ae3c856392e7c8fe6f27c2ad69cd4fb99279ba7
bluyam <bluyam@tamu.edu>
2016-03-11 21:56:48 -0600

    fixed port

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ app.get('/', function(req, res)
 	res.sendFile('/index.html');
 });
 
-app.listen(3000, function()
-{
-	console.log("Listening!");
+app.set('port', (process.env.PORT || 5000));
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
