8670af7af54afe91d00c10a50b8ed9ad78720903
bluyam <bluyam@tamu.edu>
2016-03-11 21:51:21 -0600

    fixed static path

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 express = require('express');
 path = require('path');
 app = express();
+app.use(express.static(__dirname));
 
 app.get('/', function(req, res)
 {
-	res.sendFile(path.join(__dirname+'/index.html'));
+	res.sendFile('/index.html');
 });
 
 app.listen(3000, function()
